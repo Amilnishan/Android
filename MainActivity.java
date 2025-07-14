9cc2c0ade5f8049418ace451a651b551d29dd554
Amilnishan <amilnishan7@gmail.com>
2025-07-14 15:17:02 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.login;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText usernameEditText = findViewById(R.id.usernameEditText);
+        EditText passwordEditText = findViewById(R.id.passwordEditText);
+        Button loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = usernameEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString().trim();
+
+                if (!username.isEmpty() && !password.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Login successfully done", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Login not done", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
